fc5257a32078fed2fc6fa6fbdd11cbb6b4ed2b3d
Goodnews Adewole <goodnews.adewole@example.com>
2023-11-07 23:50:33 +0100

    setting up my scrambledWord function

---
+++ src/utils/scrambleWord.js
@@ -0,0 +1,14 @@
+export default function scrambleWord(toScramble){
+    let scrambled;
+    toScramble = toScramble.split(" ")
+
+    return scrambled
+}
+
+function generateRandomNumber(){
+
+}
+
+function ifRandomNumberIsUsed(){
+    
+}
