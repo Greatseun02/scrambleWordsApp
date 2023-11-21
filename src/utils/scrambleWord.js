7c4bb227e37a929f377721e82c7f4fdc87a9fa4b
Goodnews Adewole <goodnews.adewole@example.com>
2023-11-22 00:27:37 +0100

    fixed bug, done with logic

--- src/utils/scrambleWord.js
+++ src/utils/scrambleWord.js
@@ -1,18 +1,33 @@
 export default function scrambleWord(toScramble){
     let scrambled;
     let preparedSentence = toScramble.split(" ")
-    scrambled = preparedSentence.map((word)=>scramble(word)).join(" ")
+    scrambled = preparedSentence.map((word)=>scramble(word, 0)).join(" ")
     return scrambled
 }
 
-function scramble(word){
-    return word
+function scramble(word, count){
+    let randomNumberRange = word.length
+    let selectedRandomNumbers = []
+    let wordSplit = word.split("")
+    let newWord = []
+    let randomNumber;
+    wordSplit.map(()=>{
+        randomNumber = generateRandomNumber(randomNumberRange, selectedRandomNumbers)
+        newWord.push(word[randomNumber])
+        selectedRandomNumbers.push(randomNumber)
+    })
+    newWord = newWord.join("")
+    if(newWord === word & count < 4){
+        newWord = scramble(word, count+1)
+    }
+    return newWord
 }
 
-function generateRandomNumber(){
-    
-}
-
-function ifRandomNumberIsUsed(){
-
+function generateRandomNumber(range, selectedRandomNumbers){
+    let randomNumber = Math.floor(Math.random() * range)
+    if (selectedRandomNumbers.includes(randomNumber)){
+        randomNumber = generateRandomNumber(range, selectedRandomNumbers)
+        return randomNumber
+    }
+    return randomNumber
 }
