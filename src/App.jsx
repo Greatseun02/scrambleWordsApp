ab16504dfb0b8bb6f65f19c871256e8888c704de
Goodnews Adewole <goodnews.adewole@example.com>
2023-11-22 00:36:15 +0100

    changed route

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/scramble" element={<Scramble />} />
+          <Route path="scramble" element={<Scramble />} />
         </Routes>
       </Router>
     </>
